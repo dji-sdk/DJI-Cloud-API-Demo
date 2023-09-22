b301d70c5f5a8eb11ab3259c0a38844b08e57106
Vincent <fswit@163.com>
2023-09-22 13:55:05 +0800

    fix: flightId是应用层提供的,应用层有自己的id构建规则,如果不是设备原因必须指定格式,最好就不要控制flightId的格式了

--- src/main/java/com/dji/sdk/cloudapi/wayline/FlighttaskUndoRequest.java
+++ src/main/java/com/dji/sdk/cloudapi/wayline/FlighttaskUndoRequest.java
@@ -14,9 +14,12 @@ import java.util.List;
  */
 public class FlighttaskUndoRequest extends BaseModel {
 
+    /**
+     * fix: flightId是应用层提供的,应用层有自己的id构建规则,如果不是设备原因必须指定格式,最好就不要控制flightId的格式了 by witcom@2023.09.22
+     */
     @NotNull
     @Size(min = 1)
-    private List<@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$") String> flightIds;
+    private List</*@Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$") */String> flightIds;
 
     public FlighttaskUndoRequest() {
     }
